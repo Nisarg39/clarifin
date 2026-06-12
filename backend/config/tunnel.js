0c856126a58451f10b0a7034959644992ec63af5
Nisarg Shah <nisarg@scriptlanes.com>
2026-06-12 09:32:27 -0700

    feat: enhance Serveo tunnel stability with auto-reconnect and keep-alive configuration

--- backend/config/tunnel.js
+++ backend/config/tunnel.js
@@ -4,8 +4,9 @@ const startTunnel = () => {
   console.log("Starting Serveo tunnel...");
   const tunnel = spawn("ssh", [
     "-o", "StrictHostKeyChecking=no",
-    "-o", "ServerAliveInterval=60",
+    "-o", "ServerAliveInterval=30",
     "-o", "ServerAliveCountMax=3",
+    "-o", "ExitOnForwardFailure=yes",
     "-R", "clarifin:80:localhost:5001",
     "serveo.net"
   ]);
@@ -25,8 +26,8 @@ const startTunnel = () => {
   });
 
   tunnel.on("close", (code) => {
-    console.log(`Tunnel process exited with code ${code}. Restarting in 5 seconds...`);
-    setTimeout(startTunnel, 5000);
+    console.log(`Tunnel process exited with code ${code}. Restarting in 30 seconds...`);
+    setTimeout(startTunnel, 30000);
   });
 
   tunnel.on("error", (err) => {
